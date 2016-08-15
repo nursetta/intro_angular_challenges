24eb24828a0ce1cc9c9feeced0fe90bbeb8757f9
nursetta <n_ursetta@yahoo.com>
2016-08-15 09:53:19 -0600

    final solutions

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,25 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+app.controller("WelcomeController", WelcomeController);
+	function WelcomeController(){
+	  this.full_name = "Nick Ursetta";
+	  this.age = 26;
+	  this.city = "Denver";
+	  this.state = "CO";
+	  this.letters = this.full_name.length-1;
+	  this.shout = function() {
+	  	return this.full_name.toUpperCase() + "!";
+	  };
+
+	}
+
+app.controller("ClassController", ClassController);
+	function ClassController(){
+	  this.class_name = "WDI-DEN-1 - White Walkers";
+	  this.enrolled_students = ["Nick", "Greg", "Alison", "Monica", "Charlie", "Holly", "Kyle", "Luis", "Maddy", "Mark", "Abe", "Jamie", "Kayce", "Matt"];
+	  this.start_date = "06/13/2016";
+	  this.end_date = "09/02/2016";
+	  this.days_left = "15";
+	}
